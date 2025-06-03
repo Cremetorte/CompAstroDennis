0ea36d6901049b87674b72bf6cf24144d3a00f21
cremetorte <deschwab@t-online.de>
2025-06-03 23:18:16 +0200

    Assignment 4 - Exercise 2 finished

--- Assignment_04/exercise2.jl
+++ Assignment_04/exercise2.jl
@@ -16,11 +16,11 @@ end
 
 # Error of Simpson Rule
 function simpson_error(f, a, b)
-    interval = LinRange(a, b, 100)
+    interval = LinRange(a, b, 400)
     d⁴f_dx⁴ = ith_derivative(4, f)  # cursed name
-    ξ = maximum(d⁴f_dx⁴, interval)  # Worst case for error
+    max_d4dx = maximum(d⁴f_dx⁴.(interval))  # Worst case for error <=> ξ is chosen to maximize error
     domain = b - a
-    return d⁴f_dx⁴(ξ) * (domain/2)^5 / 90    
+    return max_d4dx * (domain/2)^5 / 90    
 end
 
 # Trapez rule
@@ -56,7 +56,7 @@ end
 
 
 function si_simpson(x)
-    f(t) = sinc(t)
+    f(t) = sinc(t/π)   # sinc function (Julias definition uses sinc(x) = sin(πx)/πx)
     return adaptive_simpson_integral(f, 0, x)
 end
 
@@ -70,32 +70,28 @@ println("Using the adaptive Simpson's rule: \nSi(1) = $(si_simpson(1)) \nC(5) = 
 
 # Trapez Method adaptive
 
-function subdivide(a, b, N)
-    if N == 1
-        return [[a,b]]
-    end
-    interval = LinRange(a, b, N+1)
-    res = []
+# helper
+function trapez_fixed_nr_of_steps(f, a, b, N)
+    nodes = LinRange(a, b, N+1)
+    res = 0.0
     for i in 1:N
-        push!(res, [interval[i], interval[i+1]])
+        #use linrange as nodes
+        res += trapezoidal_integral(f, nodes[i], nodes[i+1])
     end
     return res
 end
 
-function adaptive_trapezoidal_rule(f, a, b, epsilon=1e-8, max_steps=1e10)
+function adaptive_trapezoidal_rule(f, a, b, epsilon=1e-8, max_steps=1e4)
     res = trapezoidal_integral(f, a, b)
 
     N = 2 # one was already done
     for i in 1:max_steps-1
-        subdivisions = subdivide(a,b,N)
-        temp_res = 0
-        for i in subdivisions
-            temp_res += trapezoidal_integral(f, i[1], i[2])
-        end
+        temp_res = trapez_fixed_nr_of_steps(f, a, b, N)
 
         if abs(temp_res - res) < epsilon
             return temp_res
         end
+        res = temp_res
 
         N *= 2
     end
@@ -104,8 +100,11 @@ end
 
 
 
+
+
+
 function si_trapez(x)
-    f(t) = sinc(t)
+    f(t) = sinc(t/π)   # sinc function (Julias definition uses sinc(x) = sin(πx)/πx)
     return adaptive_trapezoidal_rule(f, 0, x)
 end
 
@@ -114,4 +113,43 @@ function C_trapez(x)
     return adaptive_trapezoidal_rule(f, 0, x)
 end
 
-println("Using the adaptive Trapez rule: \nSi(1) = $(si_trapez(1)) \nC(5) = $(C_trapez(5))")
+println("Using the \"adaptive Trapez rule\": \nSi(1) = $(si_trapez(1)) \nC(5) = $(C_trapez(5))")
+
+
+try
+    # Compare to Scipys result
+    using PyCall    # Has to be installed via REPL -> Pkg mode (press "]") -> add PyCall
+
+    scipy = pyimport("scipy") # apparently this is possible 
+
+    # Second funtion; sinc is already defined in Julia
+    cos_sq(t) = cos(π/2 * t^2)
+
+    # Compute Si(1) using scipy's simpson rule
+    x_sinc = range(0, 1, length=1000) 
+    y_sinc = sinc.(x_sinc./π)
+    si_scipy = scipy.integrate.simpson(y_sinc, x_sinc)
+
+    # Compute C(5) using scipy's simpson rule
+    x_cos = range(0, 5, length=1001)
+    y_cos = cos_sq.(x_cos)
+    c_scipy = scipy.integrate.simpson(y_cos, x_cos)
+
+    println("Using scipy's Simpson rule via PyCall: \nSi(1) = $si_scipy \nC(5) = $c_scipy")
+catch ModuleNotFoundError
+    println("PyCall is not installed. Please install it via Pkg mode (press \"]\" -> add PyCall) and try again.")
+end
+
+
+
+""" Output:
+Using the adaptive Simpson's rule: 
+Si(1) = 0.8414709848078965 
+C(5) = 0.5636311907377783
+Using the "adaptive Trapez rule": 
+Si(1) = 0.9460830688712603 
+C(5) = 0.5636311867991783
+Using scipy's Simpson rule via PyCall: 
+Si(1) = 0.9460830703671914 
+C(5) = 0.5636312021714622
+"""
