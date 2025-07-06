ad205d0cc3d99cf18314569a4ba54533456199cc
cremetorte <deschwab@t-online.de>
2025-07-06 15:56:52 +0200

    added result pdf to Assignment 8

--- Assignment_08/main.jl
+++ Assignment_08/main.jl
@@ -11,6 +11,7 @@ xmax = 1
 gridpoints = 400
 σ = 0.8
 a = 1
+end_t = 4   
 
 # -----------
 # Step Widths
@@ -27,7 +28,9 @@ u_0 = [(i*Δx > -1/3 + gridpoints*Δx/2) && (i*Δx < 1/3 + gridpoints*Δx/2) ? 1
 # -----------------------------------
 # Analytical solution: shifted by a*t
 # -----------------------------------
-u_analytical = circshift(u_0, a*4)
+u_analytical = circshift(u_0, a*end_t)
+# a*end_t can only be an integer because else the rectangle peak doesnt fit onto the grid perfectly
+# But a=1, end_t=4 is the only thing needed
 
 
 # ----------------------------------
@@ -47,11 +50,11 @@ methods = Dict("Upwind" => upwind, "Centered Differences" => centered_diff, "Lax
 for method_label in keys(methods)
     # Run solving algorithm and save data in u_sol
     println("Solving Linear Advection using the $method_label-Method")
-    u_sol = solve_lin_adv((methods[method_label]), σ, u_0, Δt, 4)
+    u_sol = solve_lin_adv((methods[method_label]), σ, u_0, Δt, end_t)
 
     # run solving algorithm after it compiled when it was called above to get pure run time
     println("Benchmark results on compiled functions:")
-    @time solve_lin_adv((methods[method_label]), σ, u_0, Δt, 4)
+    @time solve_lin_adv((methods[method_label]), σ, u_0, Δt, end_t)
 
     # plot results
     plot(x_axis(u_sol), u_sol, label="Numeric Solution using the $method_label-Method", grid=true)
