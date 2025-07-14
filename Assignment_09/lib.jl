1a8ac969f58e393bdb4db48e348ef88cdfc11b39
cremetorte <deschwab@t-online.de>
2025-07-14 22:46:23 +0200

    assignment9 sync

--- Assignment_09/lib.jl
+++ Assignment_09/lib.jl
@@ -3,10 +3,17 @@ using Base.Threads
 
 c_s = 1
 
-function calculate_step!(Q1_array, Q2_array, Δt, Δx)
+function calculate_step!(Q1_array, Q2_array, Δx)
     N = length(Q1_array)
     Q1_new = similar(Q1_array)
     Q2_new = similar(Q2_array)
+
+    v_max = maximum(c_s .+ abs.(Q2_array ./ Q1_array))
+    Δt = 0.5 * Δx / v_max
+
+    Q1_half = zeros(N)
+    Q2_half = zeros(N)
+
 
     # First step of op-splitting
     @threads for i in 1:N
@@ -22,26 +29,30 @@ function calculate_step!(Q1_array, Q2_array, Δt, Δx)
         u_half_right = 0.5 * (Q2 / Q1 + Q2_right / Q1_right)
         u_half_left = 0.5 * (Q2 / Q1 + Q2_left / Q1_left)
 
-        flux1_right = u_half_right > 0 ? Q1 * u_half_right : Q1_right * u_half
-        flux2_right= u_half_right < 0 ? Q2 * u_half_right : Q2_right * u_half
+        flux1_right = u_half_right > 0 ? Q1 * u_half_right : Q1_right * u_half_right
+        flux2_right= u_half_right < 0 ? Q2 * u_half_right : Q2_right * u_half_right
 
-        flux1_left = u_half_left < 0 ? Q1 * u_half_left : Q1_left * u_half
-        flux2_left = u_half_left > 0 ? Q2 * u_half_left : Q2_left * u_half
+        flux1_left = u_half_left < 0 ? Q1 * u_half_left : Q1_left * u_half_left
+        flux2_left = u_half_left > 0 ? Q2 * u_half_left : Q2_left * u_half_left
 
-        Q1_half = Q1 - Δt/Δx * (flux1_right - flux1_left)
-        Q2_half = Q2 - Δt/Δx * (flux2_right - flux2_left)
-        
-    
-        # needs fixing
-        Q1 = Q1_array[i]
-        Q1_left = (i==1) ? Q1_array[1] : Q1_array[i-1]
-        Q1_right = (i==N) ? Q1_array[N] : Q1_array[i+1]
-
-        Q1_new[i] = Q1_array
-        Q2_new[i] = Q2_half - Δt * c_s^2 * (Q1_right - Q1_left) / (2 * Δx)
+        Q1_half[i] = Q1 - Δt/Δx * (flux1_right - flux1_left)
+        Q2_half[i] = Q2 - Δt/Δx * (flux2_right - flux2_left)
         
     end
+
+
+
+    @threads for i in 1:N
+        Q1_left = (i==1) ? Q1_half[1] : Q1_half[i-1]
+        Q1_right = (i==N) ? Q1_half[N] : Q1_half[i+1]
+
+        Q1_new[i] = Q1_half[i]
+        Q2_new[i] = Q2_half[i] - Δt * c_s^2 * (Q1_right - Q1_left) / 2*Δx 
+    end
+
     Q1_array .= Q1_new
     Q2_array .= Q2_new
+
+    return Δt # for timekeeping
     
 end
