15c27487c02dbec7613c7f481a5a0312fd230a4d
cremetorte <deschwab@t-online.de>
2025-07-18 14:24:45 +0200

    added other quantities to assignment 9

--- Assignment_09/Part2/animator.jl
+++ Assignment_09/Part2/animator.jl
@@ -1,4 +1,7 @@
 using Plots
+using Base.Threads
+
+
 try
     using DataFrames
     using CSV
@@ -23,8 +26,8 @@ gr() # Use the GR backend for plotting
 x(index) = (index-1) * 0.2
 
 
-filename = "output.csv"
-
+quantity = "internal_energy"
+filename = "Output/output_$quantity.csv"
 
 df = CSV.read(filename, DataFrame)
 data = Matrix(df)
@@ -38,15 +41,32 @@ nr_frames = size(data)[1]
 gif_length = 10 # seconds
 
 
+ylim = maximum(ρ) * 1.1
+ymin = minimum(ρ) * 0.9
+
+
 # Create a GIF
-anim = @animate for i in 1:10:nr_frames
-    print("Calculating frame $(i) / $(nr_frames)...   \r")
-    plot(x.(eachindex(ρ[i, :])), ρ[i, :], label="t = $(round(t[i], digits=2))", xlabel="x", ylabel="Density", title="Density Evolution")
-    xlims!(0, 100)
-    ylims!(1, 2.5)
+
+
+frames = Vector{Plots.Plot}(undef, nr_frames)
+
+Threads.@threads for i in 1:nr_frames
+    frames[i] = plot(
+        x.(eachindex(ρ[i, :])), ρ[i, :],
+        label="t = $(round(t[i], digits=2))",
+        xlabel="x", ylabel="$quantity",
+        title="$quantity evolution",
+        legend=:topright,
+        ylim=(ymin, ylim), xlims=(0, 100)
+    )
+end
+
+anim = @animate for i in 1:nr_frames
+    print("Rendering frame $(i) / $(nr_frames)...   \r")
+    plot!(frames[i])
 end
 
 
 println("\nAnimation complete! Saving to file...")
-gif(anim, "Animation/density_evolution.gif", fps = round(nr_frames / 10 / gif_length, digits=0))
+gif(anim, "Animation/anim_$quantity.gif", fps = round(nr_frames / gif_length, digits=0))
 
--- Assignment_09/Part2/main.jl
+++ Assignment_09/Part2/main.jl
@@ -1,21 +1,26 @@
 using Plots
+using Base.Threads
+
 try
     using DataFrames
     using CSV
+    using Dates
 catch
-    println("Could not import all necessary Packages. Do you wish to install DataFrames and CSV right now?")
+    println("Could not import all necessary Packages. Do you wish to install Dates, DataFrames and CSV right now?")
     println("type y or n: ")
     usrinput = readline()
     if usrinput == "y"
         using Pkg
         Pkg.add("DataFrames")
         Pkg.add("CSV")
+        Pkg.add("Dates")
     else
         error("Packages could not be installed")
     end
 end
 using DataFrames
 using CSV
+using Dates
 include("lib.jl")
 
 # ---------------------
@@ -47,21 +52,45 @@ savefig("Plots/Q1_initial.png")
 
 
 
-output = Matrix{Float64}(undef, 0, 1 + gridpoints)
+println("Starting simulation with $(Threads.nthreads()) threads...")
+
+start_time = now()
+
+output_rho = Matrix{Float64}(undef, 0, 1 + gridpoints)
+output_momentum = Matrix{Float64}(undef, 0, 1 + gridpoints)
+output_internal_energy = Matrix{Float64}(undef, 0, 1 + gridpoints)
 t = 0
 while t < t_end
-    global t, output
+    global t, output_rho, output_momentum, output_internal_energy
     t += calculate_step!(Q_1, Q_2, Q_3, Δx, γ)
-    row = [t; Q_1]
-    output = vcat(output, row')
+
+    row_rho = [t; Q_1]
+    output_rho = vcat(output_rho, row_rho')
+
+    row_momentum = [t; Q_2]
+    output_momentum = vcat(output_momentum, row_momentum')
+    
+    row_internal_energy = [t; Q_3]
+    output_internal_energy = vcat(output_internal_energy, row_internal_energy')
 end
+
+end_time = now()
+println("Simulation block elapsed time: ", end_time - start_time)
 
 plot(x.(eachindex(Q_1)),Q_1)
 savefig("Plots/Q1_final.png")
 
 
 # -----------
-# File Output
+# File output
 # -----------
-df = DataFrame(output, ["time", ["ρ_$i" for i in 1:gridpoints]...])
-CSV.write("Output/output.csv", df)
+df = DataFrame(output_rho, ["time", ["ρ_$i" for i in 1:gridpoints]...])
+CSV.write("Output/output_rho.csv", df)
+
+df = DataFrame(output_momentum, ["time", ["ρ_$i" for i in 1:gridpoints]...])
+CSV.write("Output/output_momentum.csv", df)
+
+df = DataFrame(output_internal_energy, ["time", ["ρ_$i" for i in 1:gridpoints]...])
+CSV.write("Output/output_internal_energy.csv", df)
+
+println("Simulation complete! Data saved to CSV files.")
